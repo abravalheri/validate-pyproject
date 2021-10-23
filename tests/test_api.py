a7cc60c451f81b3b7d98fd7d5ea2031e9f455901
Anderson Bravalheri <andersonbravalheri@gmail.com>
2021-10-23 23:43:43 +0100

    Add tests for exceptions

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,6 +1,9 @@
 from collections.abc import Mapping
+from functools import partial, wraps
 
-from validate_pyproject import api, plugins
+import pytest
+
+from validate_pyproject import api, errors, plugins, types
 
 
 def test_load():
@@ -12,12 +15,42 @@ def test_load():
     assert spec["$id"] == "https://www.python.org/dev/peps/pep-0621/"
 
 
-def test_schema_registry():
-    plg = plugins.list_from_entry_points()
-    registry = api.SchemaRegistry(plg)
-    main_schema = registry.main
-    project = main_schema["properties"]["project"]
-    assert project["$ref"] == "https://www.python.org/dev/peps/pep-0621/"
-    tool = main_schema["properties"]["tool"]
-    assert "setuptools" in tool["properties"]
-    assert "$ref" in tool["properties"]["setuptools"]
+class TestRegistry:
+    def test_with_plugins(self):
+        plg = plugins.list_from_entry_points()
+        registry = api.SchemaRegistry(plg)
+        main_schema = registry.main
+        project = main_schema["properties"]["project"]
+        assert project["$ref"] == "https://www.python.org/dev/peps/pep-0621/"
+        tool = main_schema["properties"]["tool"]
+        assert "setuptools" in tool["properties"]
+        assert "$ref" in tool["properties"]["setuptools"]
+
+    def fake_plugin(self, name, schema_version=7):
+        schema = {
+            "$id": f"https://example.com/{name}.schema.json",
+            "$schema": f"http://json-schema.org/draft-{schema_version:02d}/schema",
+            "type": "object",
+        }
+        return types.Schema(schema)
+
+    def test_incompatible_versions(self):
+        fn = wraps(self.fake_plugin)(partial(self.fake_plugin, schema_version=8))
+        plg = plugins.PluginWrapper("plugin", fn)
+        with pytest.raises(errors.InvalidSchemaVersion):
+            api.SchemaRegistry([plg])
+
+    def test_duplicated_id(self):
+        plg = [plugins.PluginWrapper("plg", self.fake_plugin) for _ in range(2)]
+        with pytest.raises(errors.SchemaWithDuplicatedId):
+            api.SchemaRegistry(plg)
+
+    def test_missing_id(self):
+        def _fake_plugin(name):
+            plg = self.fake_plugin(name)
+            del plg["$id"]
+            return plg
+
+        plg = plugins.PluginWrapper("plugin", _fake_plugin)
+        with pytest.raises(errors.SchemaMissingId):
+            api.SchemaRegistry([plg])
---
+++ tests/test_plugins.py
@@ -0,0 +1,75 @@
+# The code in this module is mostly borrowed/adapted from PyScaffold and was originally
+# published under the MIT license
+# The original PyScaffold license can be found in 'NOTICE.txt'
+
+import sys
+
+import pytest
+
+from validate_pyproject import plugins
+from validate_pyproject.plugins import ENTRYPOINT_GROUP, ErrorLoadingPlugin
+
+EXISTING = (
+    "setuptools",
+    "distutils",
+)
+
+
+if sys.version_info[:2] >= (3, 8):
+    # TODO: Import directly (no need for conditional) when `python_requires = >= 3.8`
+    from importlib.metadata import EntryPoint  # pragma: no cover
+else:
+    from importlib_metadata import EntryPoint  # pragma: no cover
+
+
+def test_load_from_entry_point__error():
+    # This module does not exist, so Python will have some trouble loading it
+    # EntryPoint(name, value, group)
+    entry = "mypkg.SOOOOO___fake___:activate"
+    fake = EntryPoint("fake", entry, ENTRYPOINT_GROUP)
+    with pytest.raises(ErrorLoadingPlugin):
+        plugins.load_from_entry_point(fake)
+
+
+def test_iterate_entry_points():
+    plugin_iter = plugins.iterate_entry_points()
+    assert hasattr(plugin_iter, "__iter__")
+    pluging_list = list(plugin_iter)
+    assert all([isinstance(e, EntryPoint) for e in pluging_list])
+    name_list = [e.name for e in pluging_list]
+    for ext in EXISTING:
+        assert ext in name_list
+
+
+def test_list_from_entry_points():
+    # Should return a list with all the plugins registered in the entrypoints
+    pluging_list = plugins.list_from_entry_points()
+    orig_len = len(pluging_list)
+    assert all(callable(e.load_fn) for e in pluging_list)
+    plugin_names = " ".join(e.tool for e in pluging_list)
+    for example in EXISTING:
+        assert example in plugin_names
+
+    # a filtering function can be passed to avoid loading plugins that are not needed
+    pluging_list = plugins.list_from_entry_points(
+        filtering=lambda e: e.name != "setuptools"
+    )
+    plugin_names = " ".join(e.tool for e in pluging_list)
+    assert len(pluging_list) == orig_len - 1
+    assert "setuptools" not in plugin_names
+
+
+class TestPluginWrapper:
+    def test_empty_help_text(self):
+        def _fn1(_):
+            return {}
+
+        pw = plugins.PluginWrapper("name", _fn1)
+        assert pw.help_text == ""
+
+        def _fn2(_):
+            """Help for `${tool}`"""
+            return {}
+
+        pw = plugins.PluginWrapper("name", _fn2)
+        assert pw.help_text == "Help for `name`"
