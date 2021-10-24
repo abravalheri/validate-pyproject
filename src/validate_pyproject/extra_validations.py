37fa5e62e541d219d0dc903e6b518acb4fea1eda
Anderson Bravalheri <andersonbravalheri@gmail.com>
2021-10-24 15:58:29 +0100

    Remove unused check for missing version
    The JSON Schema can do this check now alone, via `if-then` clause.

--- src/validate_pyproject/extra_validations.py
+++ src/validate_pyproject/extra_validations.py
@@ -10,24 +10,6 @@ from fastjsonschema import JsonSchemaValueException
 T = TypeVar("T", bound=Mapping)
 
 
-class MissingStaticOrDynamic(JsonSchemaValueException):
-    """According to PEP 621:
-
-        If the core metadata specification lists a field as "Required", then
-        the metadata MUST specify the field statically or list it in dynamic
-
-    In turn, `core metadata`_ defines:
-
-        The required fields are: Metadata-Version, Name, Version.
-        All the other fields are optional.
-
-    Since ``Metadata-Version`` is defined by the build back-end, ``name`` and
-    ``version`` are the only mandatory information in ``pyproject.toml``.
-
-    .. _core metadata: https://packaging.python.org/specifications/core-metadata/
-    """
-
-
 class RedefiningStaticFieldAsDynamic(JsonSchemaValueException):
     """According to PEP 621:
 
@@ -39,12 +21,6 @@ class RedefiningStaticFieldAsDynamic(JsonSchemaValueException):
 def validate_project_dynamic(pyproject: T) -> T:
     project_table = pyproject.get("project", {})
     dynamic = project_table.get("dynamic", [])
-    # we need to ensure `version` (`name` is already mandatory in the JSON Schema)
-    if "version" not in project_table and "version" not in dynamic:
-        msg = "You need to provide a value for `project.version` or "
-        msg += "list it under `project.dynamic`"
-        name = "data.project.version"
-        raise MissingStaticOrDynamic(msg, name=name, rule="PEP 621")
 
     for field in dynamic:
         if field in project_table:
