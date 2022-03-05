0f3fee69cda6a259f7859d4af8128bda4c1ae18c
Anderson Bravalheri <andersonbravalheri@gmail.com>
2022-03-05 13:20:14 +0000

    Ignore coverage on debug branch

--- src/validate_pyproject/error_reporting.py
+++ src/validate_pyproject/error_reporting.py
@@ -62,7 +62,8 @@ class ValidationError(JsonSchemaValueException):
     def _from_jsonschema(cls, ex: JsonSchemaValueException):
         formatter = _ErrorFormatting(ex)
         obj = cls(str(formatter), ex.value, formatter.name, ex.definition, ex.rule)
-        if os.getenv("JSONSCHEMA_DEBUG_CODE_GENERATION", "false").lower() != "false":
+        debug_code = os.getenv("JSONSCHEMA_DEBUG_CODE_GENERATION", "false").lower()
+        if debug_code != "false":  # pragma: no cover
             obj.__cause__, obj.__traceback__ = ex.__cause__, ex.__traceback__
         obj._original_message = ex.message
         obj.summary = formatter.summary
