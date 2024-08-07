e9f45f8cdb7391b1d61b33b83c29d703bf2281e3
Dimitri Papadopoulos <3234522+DimitriPapadopoulos@users.noreply.github.com>
2024-08-07 18:05:33 +0200

    Silence new MyPy errors

--- src/validate_pyproject/formats.py
+++ src/validate_pyproject/formats.py
@@ -83,7 +83,9 @@ try:
         from packaging import requirements as _req
     except ImportError:  # pragma: no cover
         # let's try setuptools vendored version
-        from setuptools._vendor.packaging import requirements as _req  # type: ignore
+        from setuptools._vendor.packaging import (  # type: ignore[no-redef]
+            requirements as _req,
+        )
 
     def pep508(value: str) -> bool:
         """See :ref:`PyPA's dependency specifiers <pypa:dependency-specifiers>`
