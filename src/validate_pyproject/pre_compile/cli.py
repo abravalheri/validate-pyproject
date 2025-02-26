407e378802013263048dcbc3403e250ed558f996
Henry Schreiner <henryschreineriii@gmail.com>
2025-02-26 16:42:46 -0500

    refactor: use PluginProtocol instead of Union when approriate
    Signed-off-by: Henry Schreiner <henryschreineriii@gmail.com>

--- src/validate_pyproject/pre_compile/cli.py
+++ src/validate_pyproject/pre_compile/cli.py
@@ -7,10 +7,10 @@ import sys
 from functools import partial, wraps
 from pathlib import Path
 from types import MappingProxyType
-from typing import Any, Dict, List, Mapping, NamedTuple, Sequence, Union
+from typing import Any, Dict, List, Mapping, NamedTuple, Sequence
 
 from .. import cli
-from ..plugins import PluginProtocol, PluginWrapper, StoredPlugin
+from ..plugins import PluginProtocol, PluginWrapper
 from ..plugins import list_from_entry_points as list_plugins_from_entry_points
 from ..remote import RemotePlugin, load_store
 from . import pre_compile
@@ -86,7 +86,7 @@ class CliParams(NamedTuple):
 
 
 def parser_spec(
-    plugins: Sequence[Union[PluginWrapper, StoredPlugin]],
+    plugins: Sequence[PluginProtocol],
 ) -> Dict[str, dict]:
     common = ("version", "enable", "disable", "verbose", "very_verbose")
     cli_spec = cli.__meta__(plugins)
